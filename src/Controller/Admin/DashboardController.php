ec1f0b7d7edbbc0058e5e5cd7bad559483922def
l00ma <looma@protonmail.ch>
2022-11-22 18:10:21 +0100

    depart projet:easyadminbundle fonctionnel

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Entity\Currency;
+use App\Entity\Photo;
+use App\Entity\Product;
+use App\Entity\User;
+
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator
+            ->setController(UserCrudController::class)
+            ->generateUrl();
+
+        return $this->redirect($url);
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Mystock');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Category');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bar')->setSubItems([
+            MenuItem::linkToCrud('Create Category', 'fas fa-plus-circle', Category::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show Categories', 'fas fa-eye', Category::class),
+        ]);
+
+        yield MenuItem::section('Currency');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bar')->setSubItems([
+            MenuItem::linkToCrud('Create Currency', 'fas fa-plus-circle', Currency::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show Currencies', 'fas fa-eye', Currency::class),
+        ]);
+
+        yield MenuItem::section('Photo');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bar')->setSubItems([
+            MenuItem::linkToCrud('Create Photo', 'fas fa-plus-circle', Photo::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show Photos', 'fas fa-eye', Photo::class),
+        ]);
+
+        yield MenuItem::section('Product');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bar')->setSubItems([
+            MenuItem::linkToCrud('Create Product', 'fas fa-plus-circle', Product::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show Products', 'fas fa-eye', Product::class),
+        ]);
+
+        yield MenuItem::section('User');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bar')->setSubItems([
+            MenuItem::linkToCrud('Create User', 'fas fa-plus-circle', User::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Show Users', 'fas fa-eye', User::class),
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    private ?Category $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $brand = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $u_weight = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $price = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $limit_date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $location = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $remark = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?Photo $photo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getUWeight(): ?int
+    {
+        return $this->u_weight;
+    }
+
+    public function setUWeight(?int $u_weight): self
+    {
+        $this->u_weight = $u_weight;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getLimitDate(): ?\DateTimeInterface
+    {
+        return $this->limit_date;
+    }
+
+    public function setLimitDate(\DateTimeInterface $limit_date): self
+    {
+        $this->limit_date = $limit_date;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getRemark(): ?string
+    {
+        return $this->remark;
+    }
+
+    public function setRemark(?string $remark): self
+    {
+        $this->remark = $remark;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?Photo
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?Photo $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
