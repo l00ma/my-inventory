8499994c9cc5d9a23781a64c480fdb825776f288
l00ma <looma@protonmail.ch>
2023-07-20 10:48:32 +0200

    add fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
